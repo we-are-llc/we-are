fc65ca7a71fdc2ed55e1c22d11cf49f461248d6c
we-are-llc <46282587+we-are-llc@users.noreply.github.com>
2026-03-05 00:18:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const targets = document.querySelectorAll(".fade-in");
+const animTargets = document.querySelectorAll(".anim");
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -13,4 +14,18 @@ const observer = new IntersectionObserver(
   }
 );
 
+const animObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-animated");
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
 targets.forEach(target => observer.observe(target));
+animTargets.forEach(target => animObserver.observe(target));
